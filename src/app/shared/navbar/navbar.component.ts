594fdf33c39a5e3f8f163d52f256800077b682f2
Anselme Dambé SANWOGOU <ansthaanselme@gmail.com>
2023-07-29 14:34:42 +0000

    Ajout du bouton de connexion sur la page d'accueil et redirection sur la page de connexion lors du clique sur le bouton

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Router } from '@angular/router';
+
 
 @Component({
     selector: 'app-navbar',
@@ -10,14 +12,24 @@ export class NavbarComponent implements OnInit {
     private toggleButton: any;
     private sidebarVisible: boolean;
 
-    constructor(public location: Location, private element : ElementRef) {
+    
+
+    constructor(public location: Location, private element : ElementRef,private router: Router) {
         this.sidebarVisible = false;
     }
+
+    
 
     ngOnInit() {
         const navbar: HTMLElement = this.element.nativeElement;
         this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
     }
+
+    //Ouverture de la page de connexion
+    ouvrirPageConnexion() {
+        // Utiliser le routeur pour naviguer vers le nouveau composant
+        this.router.navigate(['/signup']);
+      }
     sidebarOpen() {
         const toggleButton = this.toggleButton;
         const html = document.getElementsByTagName('html')[0];
